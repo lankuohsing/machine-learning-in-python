c1ecb4929ae8af42536a82931f34a2d3e9f7691f
lankuohsing <lankuohsing@163.com>
2022-05-05 22:02:00 +0800

    add k_sampling

--- data-processing/dataset_split.py
+++ data-processing/dataset_split.py
@@ -55,10 +55,42 @@ class MultiClassSplitter(object):
         X_folds=[]
         y_folds=[]
         skf = StratifiedKFold(n_splits=n_splits,random_state=random_state)
-        for train, test in skf.split(X_list, y_list):
+        for train_index, test_index in skf.split(X_list, y_list):
             X_fold_temp=[]
             y_fold_temp=[]
-            for index in test:
+            for index in test_index:
+                X_fold_temp.append(X_list[index])
+                y_fold_temp.append(y_list[index])
+            X_folds.append(X_fold_temp)
+            y_folds.append(y_fold_temp)
+        return X_folds, y_folds
+    def stratified_k_sampling(self, n_splits, drop_minority=False, random_state=None):
+        major_labels=[]
+        minor_labels=[]
+        for label, num in self.dict_multilabel_num.items():
+            if num < n_splits:
+                logging.warning("Label {} only has {} samples!".format(label,num))
+                minor_labels.append(label)
+            else:
+                major_labels.append(label)
+        if not drop_minority:#保留所有类别样本
+            X_list=self.X_list
+            y_list=self.y_list
+        else:#丢弃样本数小于n_splits的类别
+            for label in minor_labels:
+                logging.warning("Dropping label {}!".format(label))
+            X_list=[]
+            y_list=[]
+            for label in major_labels:
+                X_list.extend(self.dict_multilabel_samples[label]["X"])
+                y_list.extend(self.dict_multilabel_samples[label]["y"])
+        X_folds=[]
+        y_folds=[]
+        skf = StratifiedKFold(n_splits=n_splits,random_state=random_state)
+        for train_index, test_index in skf.split(X_list, y_list):
+            X_fold_temp=[]
+            y_fold_temp=[]
+            for index in train_index:
                 X_fold_temp.append(X_list[index])
                 y_fold_temp.append(y_list[index])
             X_folds.append(X_fold_temp)
@@ -109,7 +141,7 @@ if __name__=="__main__":
     y=[0,0,0,0,0,0,0,0,1,1]
 
     splitter=MultiClassSplitter(X,y)
-    X_folds, y_folds=splitter.stratified_k_fold(3, drop_minority=True)
+    X_folds, y_folds=splitter.stratified_k_sampling(3, drop_minority=False)
     print(X_folds)
     print(y_folds)
     X=[[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], [11, 12]]
